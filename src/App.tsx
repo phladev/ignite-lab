4df307b1bd0aba2151304746c267de44c4665ff7
Pedro Henrique <93714224+pxdrobdf@users.noreply.github.com>
2022-06-24 21:00:54 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ApolloProvider } from "@apollo/client"
 import { BrowserRouter } from "react-router-dom"
 import { client } from "./lib/apollo"
 import { Event } from "./pages/Event"
-import { Router } from "./router"
+import { Router } from "./Router"
 
 function App() {
   return (
